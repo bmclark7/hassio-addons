9539a615d27e7c02cab36feb5cd4457674eb00c0
Brett <brett.clark@kustomtech.net>
2020-10-19 15:21:44 -0400

    mod start.sh for emqx

--- emqx/rootfs/usr/bin/start.sh
+++ emqx/rootfs/usr/bin/start.sh
@@ -3,9 +3,9 @@ set -e -u
 
 emqx_exit(){
     # At least erlang.log.1 exists
-    if [ -f /config/.storage/emqx/log/erlang.log.1 ]; then
+    if [ -f /root/.git/emqx-rel/_build/emqx/rel/emqx/log/erlang.log.1 ]; then
         # tail emqx.log.*
-        erlang_log=$(echo $(ls -t /config/.storage/emqx/log/erlang.log.*) | awk '{print $1}')
+        erlang_log=$(echo $(ls -t /root/.git/emqx-rel/_build/emqx/rel/emqx/log/erlang.log.*) | awk '{print $1}')
         num=$(sed -n -e '/LOGGING STARTED/=' ${erlang_log} | tail -1)
         [ ! -z $num ] && [ $num -gt 2 ] && tail -n +$(expr $num - 2) ${erlang_log}
     fi
@@ -20,7 +20,7 @@ emqx_exit(){
 trap "emqx_exit" EXIT
 
 # Start and run emqx, and when emqx crashed, this container will stop
-/config/.storage/emqx/bin/emqx start
+/root/.git/emqx-rel/_build/emqx/rel/emqx/bin/emqx start
 
 # Sleep 5 seconds to wait for the loaded plugins catch up.
 sleep 5
@@ -30,22 +30,22 @@ echo "['$(date -u +"%Y-%m-%dT%H:%M:%SZ")']:emqx start"
 ## Fork tailing erlang.log, the fork is not killed after this script exits
 ## The assumption is that this is the docker entrypoint,
 ## hence docker container is terminated after entrypoint exists
-tail -f /config/.storage/emqx/log/erlang.log.1 &
+tail -f /root/.git/emqx-rel/_build/emqx/rel/emqx/log/erlang.log.1 &
 
 # monitor emqx is running, or the docker must stop to let docker PaaS know
 # warning: never use infinite loops such as `` while true; do sleep 1000; done`` here
 #          you must let user know emqx crashed and stop this container,
 #          and docker dispatching system can known and restart this container.
 IDLE_TIME=0
-MGMT_CONF='/config/.storage/emqx/etc/plugins/emqx_management.conf'
+MGMT_CONF='/root/.git/emqx-rel/_build/emqx/rel/emqx/etc/plugins/emqx_management.conf'
 MGMT_PORT=$(sed -n -r '/^management.listener.http[ \t]=[ \t].*$/p' $MGMT_CONF | sed -r 's/^management.listener.http = (.*)$/\1/g')
 while [ $IDLE_TIME -lt 5 ]; do
     IDLE_TIME=$(expr $IDLE_TIME + 1)
     if curl http://localhost:${MGMT_PORT}/status >/dev/null 2>&1; then
         IDLE_TIME=0
         # Print the latest erlang.log
-        now_erlang_log=$(ps -ef |grep "tail -f /config/.storage/emqx/log/erlang.log" |grep -v grep | sed -r "s/.*tail -f (.*)/\1/g")
-        new_erlang_log="$(ls -t /config/.storage/emqx/log/erlang.log.* | head -1)"
+        now_erlang_log=$(ps -ef |grep "tail -f /root/.git/emqx-rel/_build/emqx/rel/emqx/log/erlang.log" |grep -v grep | sed -r "s/.*tail -f (.*)/\1/g")
+        new_erlang_log="$(ls -t /root/.git/emqx-rel/_build/emqx/rel/emqx/log/erlang.log.* | head -1)"
         if [ $now_erlang_log != $new_erlang_log ];then
             tail -f $new_erlang_log &
             kill $(ps -ef |grep "tail -f $now_erlang_log" | grep -v grep | awk '{print $1}')
